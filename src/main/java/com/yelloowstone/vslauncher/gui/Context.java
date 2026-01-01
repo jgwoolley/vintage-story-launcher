57349b612e9471255df3cdaaa67be0ebf6551ec8
James Woolley <26191568+jgwoolley@users.noreply.github.com>
2026-01-01 07:22:42 -0500

    dynamically figure out data dir and runtime dirs

--- src/main/java/com/yelloowstone/vslauncher/gui/Context.java
+++ src/main/java/com/yelloowstone/vslauncher/gui/Context.java
@@ -19,8 +19,6 @@ public class Context {
     final ObjectMapper mapper;
     private final Stage stage;
     private final ObservableList<VintageStoryInstance> instances;
-    private final ObservableList<File> dataDirs;
-    private final ObservableList<File> runtimeDirs;
     private final VBox rootNode;
 
     public Context(final Stage stage, final VBox rootNode) {
@@ -30,9 +28,8 @@ public class Context {
         this.stage = stage;
         this.rootNode = rootNode;
         this.instances = FXCollections.observableArrayList();
-        this.dataDirs = FXCollections.observableArrayList();
-        this.runtimeDirs = FXCollections.observableArrayList();
 
+        // TODO: Save somewhere else...
         final File saveFile = new File("test.json");
 
         this.instances.addListener(new ListChangeListener<VintageStoryInstance>() {
@@ -53,55 +50,10 @@ public class Context {
 
                 // Add the loaded data back to your observable list
                 this.instances.setAll(loadedInstances);
-
-                final Set<File> dataDirs = new HashSet<>();
-                final Set<File> runtimeDirs = new HashSet<>();
-
-                for(final var instance: loadedInstances) {
-                    dataDirs.add(instance.getDataPath());
-                    runtimeDirs.add(instance.getRuntimePath());
-                }
-                this.dataDirs.addAll(dataDirs);
-                this.runtimeDirs.addAll(runtimeDirs);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        final File userHomePath = new File(System.getProperty("user.home"));
-
-        for(final File file: new File[] {
-            new File(userHomePath, "/Library/Application Support/VintagestoryData"),
-            new File(userHomePath, "/.config/VintagestoryData/"),
-            new File(userHomePath, "/.config/VintagestoryData2/"), // TODO: Remove
-            new File(userHomePath, "/.var/app/at.vintagestory.VintageStory/config/VintagestoryData/"),
-        }) {
-            if(file.exists()) {
-                this.dataDirs.add(file);
-            }
-        }        
-
-        final File vsInstancePath = new File(userHomePath, "/Documents/VSInstances");
-        final File[] vsInstancePaths = vsInstancePath.listFiles();
-        if(vsInstancePaths != null) {
-            for(final File file: vsInstancePaths) {
-                if(new File(file, "clientsettings.json").isFile()) {
-                    this.dataDirs.add(file);
-                }
-            }
-        }
-
-        // TODO: https://wiki.vintagestory.at/Vintagestory_folder
-
-        for(final File file: new File[] {
-            new File("/Applications/Vintage Story.app"),
-            new File(userHomePath, "/AppData/Roaming/Vintagestory"),
-            new File(userHomePath, "/.local/share/vintagestory"),
-        }) {
-            if(file.exists()) {
-                this.runtimeDirs.add(file);
-            }
-        }        
     }
 
     public VBox getRootNode() {
@@ -112,12 +64,62 @@ public class Context {
         return instances;
     }
 
+
     public ObservableList<File> getRuntimeDirs() {
-        return this.runtimeDirs;
+        final Set<File> runtimeDirs = new HashSet<>();
+
+        for(final var instance: this.getInstances()) {
+            runtimeDirs.add(instance.getRuntimePath());
+        }
+
+        final File userHomePath = new File(System.getProperty("user.home"));
+
+        // TODO: https://wiki.vintagestory.at/Vintagestory_folder
+
+        for(final File file: new File[] {
+                new File("/Applications/Vintage Story.app"),
+                new File(userHomePath, "/AppData/Roaming/Vintagestory"),
+                new File(userHomePath, "/.local/share/vintagestory"),
+        }) {
+            if(file.exists()) {
+                runtimeDirs.add(file);
+            }
+        }
+
+        return FXCollections.observableArrayList(runtimeDirs);
     }
 
     public ObservableList<File> getDataDirs() {
-        return this.dataDirs;
+        final Set<File> dataDirs = new HashSet<>();
+
+        for(final var instance: this.getInstances()) {
+            dataDirs.add(instance.getDataPath());
+        }
+
+        final File userHomePath = new File(System.getProperty("user.home"));
+
+        for(final File file: new File[] {
+                new File(userHomePath, "/Library/Application Support/VintagestoryData"),
+                new File(userHomePath, "/.config/VintagestoryData/"),
+                new File(userHomePath, "/.config/VintagestoryData2/"), // TODO: Remove
+                new File(userHomePath, "/.var/app/at.vintagestory.VintageStory/config/VintagestoryData/"),
+        }) {
+            if(file.exists()) {
+                dataDirs.add(file);
+            }
+        }
+
+        final File vsInstancePath = new File(userHomePath, "/Documents/VSInstances");
+        final File[] vsInstancePaths = vsInstancePath.listFiles();
+        if(vsInstancePaths != null) {
+            for(final File file: vsInstancePaths) {
+                if(new File(file, "clientsettings.json").isFile()) {
+                    dataDirs.add(file);
+                }
+            }
+        }
+
+        return FXCollections.observableArrayList(dataDirs);
     }
 
     public Stage getStage() {
