09c2fa20d42cbb537c582a59e8783cbea04ed58b
James W <26191568+jgwoolley@users.noreply.github.com>
2026-01-11 21:25:37 -0500

    Updated build workflow

--- install_deb.sh
+++ install_deb.sh
@@ -4,18 +4,24 @@ APP_NAME="vslauncher-x64"
 mkdir -p linux-resources
 cp icons/icon-128.png "linux-resources/${APP_NAME}.png"
 
+SELECTED_JAR=$(basename $(ls target/*.jar | grep -v 'original' | grep -v 'sources' | grep -v 'javadoc' | head -n 1))
+APP_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
 jpackage --input target \
     --dest output/ \
     --name "${APP_NAME}" \
-    --main-jar vintagestory-launcher-0.1.1.jar \
+    --main-jar "${SELECTED_JAR}" \
     --main-class com.yelloowstone.vslauncher.Launcher \
     --type deb \
+    --linux-package-name vslauncher \
     --linux-shortcut \
     --linux-menu-group "Game" \
     --linux-app-category "Game" \
+    --app-version "${APP_VERSION}" \
     --resource-dir linux-resources \
     --icon icons/icon-128.png
 
+sudo apt remove vslauncher-x64 --yes
 sudo apt install ./output/vslauncher-x64_1.0_amd64.deb
 
 # dpkg -c output/vslauncher-x64_1.0_amd64.deb | grep -E '\.desktop|\.png|\.svg|\.ico'
